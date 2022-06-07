0839447c38fb8602482150a70967b015049f62c4
Mateusz Paprocki <mattpap@gmail.com>
2022-06-07 13:28:46 +0200

    Let TooltipView handle initial rendering

--- bokehjs/src/lib/models/tools/inspectors/hover_tool.ts
+++ bokehjs/src/lib/models/tools/inspectors/hover_tool.ts
@@ -132,10 +132,7 @@ export class HoverToolView extends InspectToolView {
       }
     }
 
-    const views = await build_views(this._ttviews, [..._ttmodels.values()], {parent: this.plot_view})
-    for (const ttview of views) {
-      ttview.render()
-    }
+    await build_views(this._ttviews, [..._ttmodels.values()], {parent: this.plot_view})
 
     const glyph_renderers = [...(function* () {
       for (const r of computed_renderers) {
